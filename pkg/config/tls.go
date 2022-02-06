026ffd472e0921dd5307e74331d2c95843cce9ad
Chris Marslender <chrismarslender@gmail.com>
2022-02-06 13:51:11 -0600

    Add LoadPublicKeyPair function

--- pkg/config/tls.go
+++ pkg/config/tls.go
@@ -15,3 +15,14 @@ func (s *SSLConfig) LoadPrivateKeyPair() (*tls.Certificate, error) {
 	pair, err := tls.LoadX509KeyPair(path.Join(rootPath, s.PrivateCRT), path.Join(rootPath, s.PrivateKey))
 	return &pair, err
 }
+
+// LoadPublicKeyPair loads the public key pair for the SSLConfig
+func (s *SSLConfig) LoadPublicKeyPair() (*tls.Certificate, error) {
+	rootPath, err := GetChiaRootPath()
+	if err != nil {
+		return nil, err
+	}
+
+	pair, err := tls.LoadX509KeyPair(path.Join(rootPath, s.PublicCRT), path.Join(rootPath, s.PublicKey))
+	return &pair, err
+}
